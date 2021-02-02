691c25213e115a2ccc45877dcfbd79f37007ca16
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-02-02 06:43:59 -0300

    Realizando a busca nas APIs

---
+++ app.js
@@ -0,0 +1,31 @@
+const fetchPokemon = () => {
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+    
+    const pokemonPromisses = [];
+
+    for(let i = 1; i <= 150; i++){
+        pokemonPromisses.push(fetch(getPokemonUrl(i)).then(response => response.json()));   
+    }
+
+    Promise.all(pokemonPromisses)
+        .then(pokemons => {
+          //  console.log(pokemons);
+
+            const lisPokemons = pokemons.reduce((accumulator, pokemon) =>  {
+              const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+               
+                accumulator += `
+                                <li class="card">
+                                    <img class="card-image ${types[0]}" alt="${pokemon.name} src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"></img>
+                                    <h2 class="card-title">${pokemon.id}.${pokemon.name}</h2>
+                                    <p class="card-subtitle">${types.join(' | ')}</p>
+                                </li>
+                                `
+                return accumulator
+            }, '')
+
+            console.log(lisPokemons);
+        })
+}
+
+fetchPokemon();
